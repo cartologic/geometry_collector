9333da7a2e92d878bc2630518388035a731ee61a
khaledboka <bokae97@gmail.com>
2019-09-12 09:50:18 +0200

    created end point to generate the out layer

--- urls.py
+++ urls.py
@@ -5,4 +5,5 @@ from . import views, APP_NAME
 urlpatterns = [
     url(r'^$', views.index, name='%s.index' % APP_NAME),
     url(r'^check-attributes/', views.check_attributes, name='%s.check_attributes' % APP_NAME),
+    url(r'^generate/', views.generate, name='%s.generate' % APP_NAME),
 ]
--- views.py
+++ views.py
@@ -10,9 +10,9 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 from . import APP_NAME
-from .geometry_collector import check_attrs
-from .utils import create_connection_string
-
+from .geometry_collector import check_attrs, execute
+from .publishers import publish_in_geonode, publish_in_geoserver
+from .utils import create_connection_string, table_exist
 
 @login_required
 def index(request):
@@ -39,3 +39,58 @@ def check_attributes(request):
                 "result": result
             }
             return JsonResponse(json_response, status=500)
+
+
+@login_required
+def generate(request):
+    warnings = ''
+    if request.method == 'POST':
+        # 1. get the data
+        layers = [str(f) for f in json.loads(request.POST['selected_layers'])]
+        attrs = [str(f) for f in json.loads(request.POST['selected_attrs'])]
+        out_layer_name = str(request.POST['out_layer_name'])
+        # 2. Check table exist
+        if table_exist(out_layer_name):
+            json_response = {"status": False,
+                                "message": "Layer Already Exists!, Try again with different layer name, If you don't see the existing layer in the layer list, Please contact the administrator", }
+            return JsonResponse(json_response, status=500)
+        # 3. Start Generating layer
+        connection_string = create_connection_string()
+        try:
+            execute(
+                connection_string=connection_string,
+                in_layers=layers,
+                out_layer_name=out_layer_name,
+                attrs=attrs,
+            )
+        except Exception as e:
+            # TODO: roll back delete layer(table) from database if created!
+            ogr_error = 'Error while creating out Layer: {}'.format(e)
+            json_response = {"status": False,
+                                "message": "Error While Creating Out Layer In The Database! Try again or contact the administrator \n\n ogr_error:{}".format(ogr_error), }
+            return JsonResponse(json_response, status=500)
+        # 4. Create GeoServer
+        try:
+            publish_in_geoserver(out_layer_name)
+        except:
+            # TODO: roll back the database table here!
+            json_response = {
+                "status": False, "message": "Could not publish to GeoServer, Try again or contact the administrator", 'warnings': warnings}
+            return JsonResponse(json_response, status=500)
+
+        # 5. GeoNode Publish
+        try:
+            layer = publish_in_geonode(out_layer_name, owner=request.user)
+        except:
+            # TODO: roll back the delete geoserver record and db name
+            json_response = {
+                "status": False, "message": "Could not publish in GeoNode, Try again or contact the administrator", 'warnings': warnings}
+            return JsonResponse(json_response, status=500)
+
+        json_response = {"status": True, "message": "Line Layer Created Successfully!",
+                            'warnings': warnings, "layer_name": layer.alternate}
+        return JsonResponse(json_response, status=200)
+
+    json_response = {"status": False,
+                        "message": "Error While Publishing!", }
+    return JsonResponse(json_response, status=500)
