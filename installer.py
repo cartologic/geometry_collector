695a3c35b3b028dc7e89dde580019ad38f228ae8
khaledboka <bokae97@gmail.com>
2019-09-24 08:37:18 +0200

    fix typo

--- installer.py
+++ installer.py
@@ -4,9 +4,9 @@ info = {
     "title": "Geometry Collection",
     "description": "This tool allows user to collect geometries from diffrent layers with the same feature type",
     "author": 'Cartologic',
-    "home_page": 'http://cartoview.org/apps/point_to_line',
-    "help_url": "http://cartoview.org/apps/point_to_line/help/",
-    "tags": ['app', 'map', 'point_to_line'],
+    "home_page": 'http://cartoview.org/apps/geometry_collector',
+    "help_url": "http://cartoview.org/apps/geometry_collector/help/",
+    "tags": ['app', 'map', 'geometry_collector'],
     "licence": 'BSD',
     "author_website": "http://www.cartoview.org",
     "single_instance": True
